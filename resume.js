3f6bb85a040eaf5f59dd0e21e492220e4815fe3d
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2022-12-20 10:08:50 +0530

    New

---
+++ resume.js
@@ -0,0 +1,53 @@
+const myResume = {
+  "Contact": {
+    "Name": "Karthik",
+    "Mobile": 9876543210,
+    "email": "karthikm@engineer.com",
+    "Address": "Saidapet, Chennai"
+  },
+  "Profile": "To inculcate myself in a progressive organization.",
+  "Education": {
+    "SSLC": {
+      "Examination": "SSLC",
+      "Board": "Matriculation",
+      "Name of istitution": "Anna Gem",
+      "Percentage": 77,
+      "Year of completion": 2011
+    },
+    "HSC": {
+      "Examination": "HSC",
+      "Board": "State",
+      "Name of istitution": "Anna Gem",
+      "Percentage": 76,
+      "Year of completion": 2013
+    },
+    "Degree": {
+      "Examination": "B.E.(ECE)",
+      "Board": "Anna University",
+      "Name of istitution": "Dhanalakshmi Srinivasan",
+      "Percentage": 61,
+      "Year of completion": 2017
+    }
+  },
+  "Skills": {
+    "Programming Languages Known": "HTML, css, ReactJS, Python",
+    "Software known": "Advanced Microsoft Excel",
+    "Typewriting": "English - SO words per minute"
+  },
+  "Professional Experience": {
+    "August 2022 - Present": {
+        "Company Name": "Omega Healthcare",
+        "Designation": "Process Executive"
+    },
+    "July 2018 - July 2022": {
+        "Company Name": "Tamil Nadu Health System Project",
+        "Designation": "Secretarial Assistant"
+    },
+    "October 2017 - May 2018": {
+        "Company Name": "Redisolve Software Private Ltd",
+        "Designation": "e-Relationship Associate"
+    }
+  },
+  "Project Presentation": "ROBOTS DRIVEN BY EEG SOURCE IMAGING"
+}
+console.log(myResume)
